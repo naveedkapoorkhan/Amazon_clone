dba07c7b79982326e224784361be16a3a98da544
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2024-10-24 06:10:25 -0700

    further code is added for remaining left right button

---
+++ script.js
@@ -0,0 +1,132 @@
+const RightButton=document.querySelector(".r-btn");
+const LeftButton=document.querySelector(".l-btn");
+const Right2Button=document.querySelector(".r2-btn");
+const Left2Button=document.querySelector(".l2-btn");
+const Right3Button=document.querySelector(".r3-btn");
+const Left3Button=document.querySelector(".l3-btn");
+const Right4Button=document.querySelector(".r4-btn");
+const Left4Button=document.querySelector(".l4-btn");
+const Right5Button=document.querySelector(".r5-btn");
+const Left5Button=document.querySelector(".l5-btn");
+const Right6Button=document.querySelector(".r6-btn");
+const Left6Button=document.querySelector(".l6-btn");
+const Right7Button=document.querySelector(".r7-btn");
+const Left7Button=document.querySelector(".l7-btn");
+
+
+
+
+RightButton.addEventListener("click",function (event) {
+        const contant=document.querySelector(".product-slide");
+        contant.scrollLeft +=1100;
+        event.preventDefault();
+        
+    }
+);
+LeftButton.addEventListener("click",
+    function (event) {
+        const contant=document.querySelector(".product-slide");
+        contant.scrollLeft -=1100;
+        event.preventDefault();
+        
+    }
+);
+
+Right2Button.addEventListener("click",function (event) {
+    const contant=document.querySelector(".product-slide2");
+    contant.scrollLeft +=1100;
+    event.preventDefault();
+    
+}
+);
+Left2Button.addEventListener("click",
+function (event) {
+    const contant=document.querySelector(".product-slide2");
+    contant.scrollLeft -=1100;
+    event.preventDefault();
+    
+}
+);
+
+Right3Button.addEventListener("click",function (event) {
+    const contant=document.querySelector(".product-slide3");
+    contant.scrollLeft +=1100;
+    event.preventDefault();
+    
+}
+);
+Left3Button.addEventListener("click",
+function (event) {
+    const contant=document.querySelector(".product-slide3");
+    contant.scrollLeft -=1100;
+    event.preventDefault();
+    
+}
+);
+
+
+
+Right4Button.addEventListener("click",function (event) {
+    const contant=document.querySelector(".product-slide4");
+    contant.scrollLeft +=1100;
+    event.preventDefault();
+    
+}
+);
+Left4Button.addEventListener("click",
+function (event) {
+    const contant=document.querySelector(".product-slide4");
+    contant.scrollLeft -=1100;
+    event.preventDefault();
+    
+}
+);
+
+Right5Button.addEventListener("click",function (event) {
+    const contant=document.querySelector(".product-slide5");
+    contant.scrollLeft +=1100;
+    event.preventDefault();
+    
+}
+);
+Left5Button.addEventListener("click",
+function (event) {
+    const contant=document.querySelector(".product-slide5");
+    contant.scrollLeft -=1100;
+    event.preventDefault();
+    
+}
+);
+
+Right6Button.addEventListener("click",function (event) {
+    const contant=document.querySelector(".product-slide6");
+    contant.scrollLeft +=1100;
+    event.preventDefault();
+    
+}
+);
+Left6Button.addEventListener("click",
+function (event) {
+    const contant=document.querySelector(".product-slide6");
+    contant.scrollLeft -=1100;
+    event.preventDefault();
+    
+}
+);
+
+Right7Button.addEventListener("click",function (event) {
+    const contant=document.querySelector(".product-slide7");
+    contant.scrollLeft +=1100;
+    event.preventDefault();
+    
+}
+);
+Left7Button.addEventListener("click",
+function (event) {
+    const contant=document.querySelector(".product-slide7");
+    contant.scrollLeft -=1100;
+    event.preventDefault();
+    
+}
+);
+
